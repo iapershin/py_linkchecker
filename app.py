00d248688321436f78762a331ab910f79fbec63a
mvth027 <yarpost@outlook.com>
2020-11-01 17:34:15 +0300

    small fixes

--- app.py
+++ app.py
@@ -10,23 +10,28 @@ app.config.from_object(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    url_list = None
-    if request.method == 'POST' and 'url_list' in request.form:
-        url_list = request.form['url_list']
-    return render_template('form.html', url_list=url_list)
+    return render_template('form.html')
 
 @app.route("/get_result", methods=['GET', 'POST'])
 def render_result():
-    input = request.form['url_list']
     urls = []
     result_array = []
+    if (str(request.form['agent_option'])=="on"):
+        user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
+    else:
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     for i in request.form['url_list'].splitlines():
       urls.append(i)
     for url in urls:
-        response = requests.get(url,allow_redirects=True)
-        result_array.append([url,response.status_code,response.url])
+        headers = {
+            'User-Agent': '{}'.format(user_agent)
+            }
+        try:
+            response = requests.get(url,allow_redirects=True,headers=headers)
+            result_array.append([url,response.status_code,response.url])
+        except requests.exceptions.ConnectionError:
+            result_array.append([url,"503","Connetion error. Host is not available"])
     return render_template('view_result.html', result=result_array)
-
 
 if __name__ == "__main__":
     app.run(host=sys.argv[1], port=int(sys.argv[2]), debug=sys.argv[3])
