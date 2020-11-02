4e44ec7fb8a2fb9387993bf6255ff52bfebb42f8
meth027 <meth@localhost.localdomain>
2020-11-02 15:38:31 -0500

    small fixes

--- app.py
+++ app.py
@@ -19,14 +19,25 @@ def render_result():
     #else:
     #    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     #user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
-    #headers = {
-    #        'User-Agent': '{}'.format(user_agent)
-    #    }
+    header = {
+        'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'accept-encoding':'gzip, deflate, br',
+        'accept-language':'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'cache-control':'no-cache',
+        'dnt': '1',
+        'pragma': 'no-cache',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+    }
+
     for i in request.form['url_list'].splitlines():
-      urls.append(i)
+      urls.append(i.rstrip())
     for url in urls:
         try:
-            response = requests.get(url,allow_redirects=True)
+            response = requests.get(url,allow_redirects=True,headers=header)
             result_array.append([url,response.status_code,response.url])
         except requests.exceptions.ConnectionError:
             result_array.append([url,"503","Connection error. Host is not available"])
