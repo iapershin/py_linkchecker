7faefaac64f27d76da2e7bd45791e54562458f0f
meth027 <meth@localhost.localdomain>
2020-11-02 15:19:46 -0500

    small fixes

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ def render_result():
         user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1"
     else:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+    headers = {
+            'User-Agent': '{}'.format(user_agent)
+        }
     for i in request.form['url_list'].splitlines():
       urls.append(i)
     for url in urls:
-        headers = {
-            'User-Agent': '{}'.format(user_agent)
-            }
         try:
-            response = requests.get(url,allow_redirects=True)
+            response = requests.get(url,allow_redirects=True,headers=headers)
             result_array.append([url,response.status_code,response.url])
         except requests.exceptions.ConnectionError:
             result_array.append([url,"503","Connection error. Host is not available"])
