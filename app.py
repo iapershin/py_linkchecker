9f3d39b56b964ddf3ca50a5df3b5d9311ca92ffa
meth027 <meth@localhost.localdomain>
2020-11-02 14:24:10 -0500

    small fixes

--- app.py
+++ app.py
@@ -19,18 +19,24 @@ def render_result():
     else:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
     for i in request.form['url_list'].splitlines():
-      urls.append(i)
+      urls.append(i.rstrip())
     for url in urls:
-        headers = {
-            'User-Agent': '{}'.format(user_agent)
-            }
-        try:
-            response = requests.head(url,allow_redirects=True,headers=headers)
-            result_array.append([url,response.status_code,response.url])
-        except requests.exceptions.ConnectionError:
-            result_array.append([url,"503","Connetion error. Host is not available"])
+        if url == '':
+            pass
+        else:
+          headers = {
+              'User-Agent': '{}'.format(user_agent)
+              }
+          try:
+              response = requests.get(url,allow_redirects=True,headers=headers)
+              result_array.append([url,response.status_code,response.url])
+          except requests.exceptions.ConnectionError:
+              result_array.append([url,"503","Connection error. Host is not available"])
+          except requests.exceptions.MissingSchema:
+              result_array.append([url,"503","Invalid url"])
+          except requests.exceptions.ConnectTimeout:
+              result_array.append([url,"408","Request timeout"])
     return render_template('view_result.html', result=result_array)
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host=sys.argv[1], port=int(sys.argv[2]))
+    app.run(host=sys.argv[1], port=int(sys.argv[2]))
